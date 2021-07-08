51ff2e0bf0ada6763902e9bc75a1e66eecbe51d5
Robert Harrison <robharrison0399@gmail.com>
2021-07-08 18:06:22 +0100

    Tried to setup user protected routes with login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import './App.css';
-import Nav from './navComponents/nav';
-import Movies from './FilmComponents/movies';
-import MovieDetail from './FilmComponents/movieDetail';
+import Nav from './components/navComponents/nav';
+import Movies from './components/filmComponents/movies';
+import MovieDetail from './components/filmComponents/movieDetail';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Account from './userComponents/account';
-import PrivateRoute from './userComponents/privateRoute';
+import Account from './components/userComponents/account';
+import PrivateRoute from './components/userComponents/privateRoute';
+import useFindUser from './hooks/useFindUser'
+import { UserContext } from './hooks/userContext';
+import Login from './pages/login';
 
 function App() {
+    const {
+        user,
+        setUser,
+        isLoading } = useFindUser();
     return (
         <Router>
+            <UserContext.Provider value={{ user, setUser, isLoading }}>
             <div className="App">
                 <Nav />
                 <Switch>
                     <Route path="/" exact />
                     <Route path="/movie" exact component={Movies} />
                     <Route path="/user" component={Account} />
+                    <Route path="/login" component={Login}/>
                     <Route path="/movie/:id" component={MovieDetail} />
                     <PrivateRoute path="/account" component={Account} />
 
                 </Switch>
             </div>
+            </UserContext.Provider>
         </Router>
     )
 }
